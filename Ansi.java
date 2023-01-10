5217a2497991332c44fd8251a251c950b5dc57b1
ohyeahsoldier <122343605+ohyeahsoldier@users.noreply.github.com>
2023-01-10 13:38:33 -0800

    Update Ansi.java

--- Ansi.java
+++ Ansi.java
@@ -1,6 +1,8 @@
 public class Ansi {
   // Reset
-  public static final String RESET = "\033[0m"; // Text Reset // Regular Colors
+  public static final String RESET = "\033[0m"; // Text Reset
+  
+  // Regular Colors
   public static final String BLACK = "\033[0;30m"; // BLACK
   public static final String RED = "\033[0;31m"; // RED
   public static final String GREEN = "\033[0;32m"; // GREEN
