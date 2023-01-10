e0a0ba9da7057b21b4f59bdef757a46b2e414d24
ohyeahsoldier <122343605+ohyeahsoldier@users.noreply.github.com>
2023-01-10 13:57:28 -0800

    Update Sheep.java

--- Sheep.java
+++ Sheep.java
@@ -1,6 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 public class Sheep {
-  public static void main(String[] args) {
-    //
-  }
+ 	public static void main (String[] args) throws java.lang.Exception {
+		String x = "😀";
+		System.out.println( x ) ;
+    
+		String y = Character.toString( 128_512 );
+		System.out.println( y ) ;
+	}
 }
