a07dcc892bee9aaaa12b10e651b0f661ad4a3e6f
ohyeahsoldier <122343605+ohyeahsoldier@users.noreply.github.com>
2023-01-11 06:49:12 +0000

    h

---
+++ Client.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Client {
+    /**
+     * @param ms number of milliseconds to sleep for
+     */
+    private static void wait(int ms) {
+        try { //.sleep() for `ms` MILLISECONDS
+            TimeUnit.MILLISECONDS.sleep(ms);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    private static Scanner input = new Scanner(System.in);
+    public static void main (String[] args)  {
+        while (true) {
+		    System.out.println(Color.GREEN_BACKGROUND + "🟩🟩🟩🐑");
+		    System.out.println(Color.GREEN_BACKGROUND + "🟩🟩🐑🟩");
+		    System.out.println(Color.GREEN_BACKGROUND + "🟩🐑🟩🟩");
+		    System.out.println(Color.GREEN_BACKGROUND + "🐑🟩🟩🟩");
+		    System.out.println(Color.GREEN_BACKGROUND + "🟩🟩🟩🟩");
+            
+            Tile[][] world = new Tile[5][5];
+
+            String INPUT = input.next();
+            switch (INPUT) {
+                case "a":
+                    return;
+                default:
+            }
+            wait(900);
+        }
+	}
+}
---
+++ Tile.java
@@ -0,0 +1,7 @@
+public class Tile {
+    private boolean grass;
+
+    public Tile() {
+        
+    }
+}
